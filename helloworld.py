1d216fea5b9e86bd10b072bb731ae125dd23fc86
Jahi Dickson <jahidickson82003@gmail.com>
2018-07-24 20:38:37 +0000

    made rockpaper scissor and fizzbuzz game

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+#First program in python
+
+name = raw_input("What is your name?")
+print ("Hello " +  name)
---
+++ rock_paper_scissors.py
@@ -0,0 +1,33 @@
+#rock paper scissors
+
+from random import randint
+player = raw_input('rock (r) paper (p) scissors (s)?')
+
+chosen = randint(1,3)
+#print(chosen)
+
+if chosen == 1:
+    computer = 'r'
+elif chosen == 2:
+    computer = 'p'
+elif chosen == 3:
+    computer = 's'
+
+print (player + ' vs ' + computer)
+
+if player == computer:
+    print ("DRAW!!!")
+elif player == 'r' and computer == 's':
+    print ("PLAYER WINS!!!")
+elif player == 's' and computer == 'p':
+    print ("PLAYER WINS!!!")
+elif player == 'p' and computer == 'r':
+    print ("PLAYER WINS!!!")
+elif player == 'r' and computer == 'p':
+    print ("COMPUTER WINS!!!")
+elif player == 'p' and computer == 's':
+    print ("COMPUTER WINS!!!")
+elif player == 's' and computer == 'r':
+    print ("COMPUTER WINS!!!")
+
+
